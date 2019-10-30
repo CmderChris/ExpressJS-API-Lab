706778bd28d07ab6e7a46d412321eb5e02bbac64
Chris Smith <christopherkeithsmith@gmail.com>
2019-10-30 16:59:49 -0500

    updated files

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const chirpsStore = require('../chirpstore.js');
+
 let router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('chirps');
+router.get('/:id?', (req, res) => {
+    let id = req.params.id;
+    if(id) {
+        res.json(chirpsStore.GetChirp(id));
+    } else {
+        res.send(chirpsStore.GetChirps());
+    }
+});
+
+router.post('/', (req, res) => {
+    chirpsStore.CreateChirp(req.body);
+    res.sendStatus(200);
 });
 
 module.exports = router;
