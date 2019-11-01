bda06d19121056f298251df2d222699391473b02
Chris Smith <christopherkeithsmith@gmail.com>
2019-11-01 12:48:24 -0500

    done?

--- client/app.js
+++ client/app.js
@@ -27,20 +27,48 @@ const displayChirps = (chirps) => {
                     <div class="card message-center">
                         <h4 class="card-title mt-3">${chirp.name} said:</h4>
                             <p class="card-message">${chirp.message}</p>
-                        <button onClick="deleteChirp(${chirp.id})" id="deleteChirp" 
-                            class="btn-outline-info w-20 mx-auto mb-3">Delete Chirp</button> 
+
+                            <button onClick="editChirp(${chirp.id},'${chirp.name}', '${chirp.message}')" id="editChirp" type="button"
+                            class="btn btn-outline-info w-20 mx-auto mb-3 btn-sm">Edit Chirp</button>
+
+                            <button onClick="deleteChirp(${chirp.id})" id="deleteChirp" type="button"
+                            class="btn btn-outline-info w-20 mx-auto mb-3 btn-sm">Delete Chirp</button> 
+                            
                     </div>
                 </div>
             </div>
         `);
     })
 }
+
 const deleteChirp = (id) => {
     $.ajax({
             type: "DELETE",
             url: `/api/chirps/${id}`
         })
         .then(chirps => displayChirps(chirps));
+};
+
+const editChirp = (id, name, message) => {
+    Swal.fire({
+        title: `Edit Chirp ${id}`,
+        input: 'text',
+        inputValue: message,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        preConfirm: (editedMessage) => {
+            updateChirp(id,name,editedMessage)
+        }
+    })
+}
+
+const updateChirp = (id, name, message) => {
+    $.ajax({
+        type: "PUT",
+        url: `/api/chirps/${id}`,  
+        data: {name, message}
+    })
+    .then(chirps => displayChirps(chirps))
 };
 
 getChirps();
@@ -60,7 +88,6 @@ $('#chirpSubmit').click(() => {
         getChirps();
     })
 });
-
 
 
 
